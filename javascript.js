4dcb7518783d7134afd7ddcb98cfa256273f9a84
AshfoMon <ashfordmonte@gmail.com>
2019-12-29 00:20:35 -0600

    Cleaned up some code. Changed br element to a smaller gap. Added images file to store album images. Added buttons that correspond to an album and show it.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//Code for band member pictures
+
 function band(){
     document.getElementById("band").src="https://ichef.bbci.co.uk/images/ic/960x540/p01bqsqd.jpg";
 }
@@ -21,3 +23,63 @@ document.getElementById("button2").addEventListener("click", geddy);
 document.getElementById("button3").addEventListener("click", lifeson);
 
 document.getElementById("button4").addEventListener("click", peart);
+
+//Code for album pictures
+
+document.getElementById("album1").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush.jpg";
+});
+document.getElementById("album2").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Fly_by_Night.jpg";
+});
+document.getElementById("album3").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Caress_of_Steel.jpg";
+});
+document.getElementById("album4").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_2112.jpg";
+});
+document.getElementById("album5").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_A_Farewell_to_Kings.jpg";
+});
+document.getElementById("album6").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Hemispheres.jpg";
+});
+document.getElementById("album7").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Permanent_Waves.jpg";
+});
+document.getElementById("album8").addEventListener("click", function(){
+    document.getElementById('album').src="images/Moving_Pictures.jpg";
+});
+document.getElementById("album9").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Signals.jpg";
+});
+document.getElementById("album10").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Grace_Under_Pressure.jpg";
+});
+document.getElementById("album11").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Power_Windows.jpg";
+});
+document.getElementById("album12").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Hold_Your_Fire.jpg";
+});
+document.getElementById("album13").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Presto.jpg";
+});
+document.getElementById("album14").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_roll_the_bones.jpg";
+});
+document.getElementById("album15").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Counterparts.jpg";
+});
+document.getElementById("album16").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Test_for_Echo.jpg";
+});
+document.getElementById("album17").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Vapor_Trails.jpg";
+});
+document.getElementById("album18").addEventListener("click", function(){
+    document.getElementById('album').src="images/Snakesandarrows.jpg";
+});
+document.getElementById("album19").addEventListener("click", function(){
+    document.getElementById('album').src="images/Rush_Clockwork_Angels_artwork.jpg";
+});
